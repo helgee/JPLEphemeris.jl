94a7c1bbfb1d2a9eee669daadf05291cc8563ced
Helge Eichhorn <mail@helgeeichhorn.de>
2013-12-02 13:43:03 +0100

    Add JPL test suite.

--- test/test.jl
+++ test/test.jl
@@ -1,23 +1,78 @@
 using JPLEphemeris
 using Base.Test
-import Base.source_path
 
-path = dirname((source_path()))
+import JPLEphemeris.state
 
-if ~isfile("$path/header.421")
-    run(`curl -o $path/header.421 ftp://ssd.jpl.nasa.gov/pub/eph/planets/ascii/de421/header.421`)
+function testephemeris(ephem::Ephemeris)
+    denum = int(ephem.constants["DENUM"])
+    if isfile("$path/testpo.$denum")
+        lines = open(readlines, "$path/testpo.$denum")
+    else
+        error("Test file 'testpo.$denum' not found.")
+    end
+
+    for l in lines
+        if ismatch(r"^[0-9]", l)
+            de, date, jd, target, center, index, value = [f(v) for (f,v) in
+            zip((string, string, float, int, int, int, float), split(l))]
+
+            if target in 14:15
+                r = state(ephem, jd, target)
+            else
+                tr = state(ephem, jd, target)
+                cr = state(ephem, jd, center)
+                r = (tr - cr)/ephem.constants["AU"]
+            end
+
+            println("Date: $date")
+            println("Julian day: $jd")
+            println("Target: $target")
+            println("Center: $center")
+            println("Orginal value: $value")
+            println("Computed value: $(r[index])")
+            println("===========================================")
+
+            if target == 15 && index == 3
+                delta = (r[index] - value)/(0.23*(jd - 2451545.0))
+                @test delta <= 1e-13
+            elseif target == 15 && index == 6
+                delta = (r[index] - value)*0.01/(1.0 + (jd - 2451545.0)/365.25)
+                @test delta <= 1e-13
+            else
+                @test isapprox(r[index], value)
+            end
+        end
+    end
 end
 
-if ~isfile("$path/ascp1900.421")
-    run(`curl -o $path/ascp1900.421 ftp://ssd.jpl.nasa.gov/pub/eph/planets/ascii/de421/ascp1900.421`)
+function state(ephem::Ephemeris, date::Float64, target::Int64)
+    s(body::String) = state(ephem, body, date)
+
+    planets = [1=>"mercury", 2=>"venus", 4=>"mars", 5=>"jupiter",
+    6=>"saturn", 7=>"uranus", 8=>"neptune", 9=>"pluto", 11=>"sun",
+    14=>"nutations", 15=>"librations"]
+
+    if target == 3
+        return s("earthmoon") - s("moon") * ephem.constants["earthshare"]
+    elseif target == 10
+        return s("earthmoon") + s("moon") * ephem.constants["moonshare"]
+    elseif target == 12
+        return zeros(6)
+    elseif target == 13
+        return s("earthmoon")
+    else
+        return s(planets[target])
+    end
 end
 
-if ~isfile("$path/ascp2050.421")
-    run(`curl -o $path/ascp2050.421 ftp://ssd.jpl.nasa.gov/pub/eph/planets/ascii/de421/ascp2050.421`)
+path = "$(Pkg.dir())/JPLEphemeris/deps"
+
+# Run the JPL testsuite for every installed ephemeris.
+for f in readdir(path)
+    file, ext = splitext(f)
+    if ext == ".jld"
+        eph = Ephemeris("$path/$f")
+        testephemeris(eph)
+    end
 end
 
-header = "$path/header.421"
-datafiles = ["$path/ascp1900.421", "$path/ascp2050.421"]
-outfile = "$path/de421.jld"
-
-coeff = readascii(header, datafiles, outfile)
